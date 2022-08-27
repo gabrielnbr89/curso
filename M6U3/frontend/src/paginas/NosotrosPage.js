6b0d453147da86152e95ad2d7d0a66f2d2d466ef
Gabriel <gabrielnbr89@gmail.com>
2022-08-27 19:32:50 -0300

    Confirmando cambios en backend y frontend

---
+++ M6U3/frontend/src/paginas/NosotrosPage.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const NosotrosPage = (props) => {
+    return(
+        <main className="holder">
+        <h2>Quienes somos</h2>
+        <hr/>
+        <div className="fila">
+            <div className="columna67">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id accusamus deserunt possimus placeat,
+                    perferendis harum dolore repellendus omnis ad quas provident recusandae eos quibusdam, modi
+                    aspernatur,
+                    temporibus similique nostrum molestiae.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit qui atque corrupti quisquam velit
+                    laborum illo quis, aperiam neque reprehenderit vero doloremque tenetur! Ad nihil corporis obcaecati
+                    a
+                    ipsum quod?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sit quia neque. Suscipit ex quos
+                    beatae
+                    rerum doloribus, dicta modi aspernatur odit maxime nostrum facere, libero minus assumenda, enim
+                    dolorum.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat sit culpa tempora aspernatur
+                    necessitatibus ad reprehenderit. Nam recusandae ea, ex animi mollitia ut corporis obcaecati minima
+                    ipsam, dolorem deleniti optio.
+
+                </p>
+            </div>
+            <div className="columna33">
+                <img src="img/edificio.png" alt=""/>
+            </div>
+        </div>
+    </main>
+    );
+}
+
+export default NosotrosPage;
